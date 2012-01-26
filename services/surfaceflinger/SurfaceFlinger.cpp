3ca605d13a84739e313c0a5032191f76dbd6b447
Naomi Luis <nluis@codeaurora.org>
2012-01-26 10:01:03 +0100

    SurfaceFlinger: Set the buffer size in the GraphicBufferAlloc
    When setting the buffer size, store the required size, and allocate
    the buffer of the required size in GraphicBufferAlloc, instead of
    SurfaceTexture. We do this because SurfaceTexture does not always
    run in the server process space. GraphicBufferAlloc always runs in the
    server process.
    
    Change-Id: I53661d8a9a27f9974837f2ed2fe13facf8dd8d9c

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2975,6 +2975,7 @@ status_t Client::destroySurface(SurfaceID sid) {
 #ifdef QCOM_HARDWARE
 GraphicBufferAlloc::GraphicBufferAlloc() {
     mFreedIndex = -1;
+    mSize = 0;
 }
 #else
 GraphicBufferAlloc::GraphicBufferAlloc() {}
@@ -2997,6 +2998,7 @@ sp<GraphicBuffer> GraphicBufferAlloc::createGraphicBuffer(uint32_t w, uint32_t h
         return 0;
     }
 #ifdef QCOM_HARDWARE
+    checkBuffer((native_handle_t *)graphicBuffer->handle, mSize, usage);
     Mutex::Autolock _l(mLock);
     if (-1 != mFreedIndex) {
         mBuffers.insertAt(graphicBuffer, mFreedIndex);
@@ -3029,6 +3031,10 @@ void GraphicBufferAlloc::freeGraphicBufferAtIndex(int bufIdx) {
      } else {
         mFreedIndex = -1;
      }
+}
+
+void GraphicBufferAlloc::setGraphicBufferSize(int size) {
+    mSize = size;
 }
 #endif
 // ---------------------------------------------------------------------------
